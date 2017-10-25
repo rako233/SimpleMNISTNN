71b12459861c7fc39d25713a3e9ef6072e4999e0
rako233 <rako233@spamgourmet.com>
2017-10-25 21:31:15 +0200

    add lr adaptation

--- mnist.py
+++ mnist.py
@@ -34,16 +34,3 @@ def read(dataset = "training", path = "/home/ralph/Documents/Jupyter/mnist/data"
 
     return lbl,img
 
-def show(image):
-    """
-    Render a given numpy.uint8 2D array of pixel data.
-    """
-    from matplotlib import pyplot
-    import matplotlib as mpl
-    fig = pyplot.figure()
-    ax = fig.add_subplot(1,1,1)
-    imgplot = ax.imshow(image, cmap=mpl.cm.Greys)
-    imgplot.set_interpolation('nearest')
-    ax.xaxis.set_ticks_position('top')
-    ax.yaxis.set_ticks_position('left')
-    pyplot.show()
